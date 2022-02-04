a78d806f928844f0170283add466b48e13f5a897
Alejandro Baez Muñoz <alejandrobaezmunoz@gmail.com>
2022-02-03 19:30:48 -0500

    Taller #3 Promedio, mediana y moda

--- README.md
+++ README.md
@@ -13,3 +13,10 @@
 - Segundo paso: Implementar formulas en JavaScript
 - Tercer paso: Crear funciones
 - Cuarto paso: Integrar JavaScript con HTML
+
+## Taller #3: Promedio, mediana y moda
+
+- Primer paso: Definir las formulas
+- Segundo paso: Implementar formulas en JavaScript
+- Tercer paso: Crear funciones
+- Cuarto paso: Integrar JavaScript con HTML
--- descuentos.js
+++ descuentos.js
@@ -1,7 +1,7 @@
 // const precioOriginal = 100;
 // const descuento = 15;
 
-function CalcularPrecioConDescuento(precio, descuento){
+function calcularPrecioConDescuento(precio, descuento){
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento)/100;
 
@@ -12,11 +12,38 @@ function onClickButtonPriceDiscount(){
     const inputPrice = document.getElementById("input-price");
     const priceValue = inputPrice.value;
     
-    const inputDiscount = document.getElementById("input-discount");
-    const discountValue = inputDiscount.value;
+    const inputCoupon = document.getElementById("input-coupon");
+    const couponValue = inputCoupon.value;
 
-    const precioConDescuento = CalcularPrecioConDescuento(priceValue, discountValue);
-
-    const resultP = document.getElementById("ResultP");
-    resultP.innerText = "El precio con descuento son $" + precioConDescuento;
+    const isCouponValueValid = function (coupon) {
+        return coupon.name === couponValue;
+    };
+    
+    const userCoupon = coupons.find(isCouponValueValid);
+    
+    if (!userCoupon) {
+        const resultP = document.getElementById("ResultP");
+        resultP.innerText = "El cupón " + couponValue + " no es válido";
+    } else {
+        const descuento = userCoupon.discount;
+        const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+    
+        const resultP = document.getElementById("ResultP");
+        resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+    }
 }
+
+const coupons = [
+    {
+        name: "JuanDC_es_Batman",
+        discount: 15,
+    },
+    {
+        name: "pero_no_le_digas_a_nadie",
+        discount: 30,
+    },
+    {
+        name: "es_un_secreto",
+        discount: 25,
+    },
+];
