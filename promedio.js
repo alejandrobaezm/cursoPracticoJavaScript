9fd8a0069453398a368215ca8782e0419a9ec865
Alejandro Baez Muñoz <alejandrobaezmunoz@gmail.com>
2022-02-04 11:43:55 -0500

    Function moda

--- promedio.js
+++ promedio.js
@@ -2,12 +2,12 @@ const lista1 = [
     100,
     200,
     300,
-    500,
-    1000000,
-    350,
-    900,
-    110,
-    550
+    100,
+    100,
+    300,
+    200,
+    200,
+    200
 ];
 
 function calcularMediaAritmetica(lista){
@@ -58,4 +58,25 @@ function calcularMediana(lista) {
     }
 
 }
+
+const lista1Count = {};
+
+lista1.map(
+    function (elemento) {
+        if (lista1Count[elemento]) {
+            lista1Count[elemento] += 1;
+        } else {
+            lista1Count[elemento] = 1;
+        }
+    }
+);
+
+const lista1Array = Object.entries(lista1Count).sort(
+    function (valorAcumulado, nuevoValor) {
+        return valorAcumulado[1] - nuevoValor[1];
+    }
+);
+
+const moda = lista1Array[lista1Array.length - 1];
+
 
