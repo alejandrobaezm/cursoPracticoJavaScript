ada5f12c8ac54ef3a060cf61da577fc9e6ab9736
Alejandro Baez Muñoz <alejandrobaezmunoz@gmail.com>
2022-02-07 19:34:46 -0500

    Taller #4 salarios

---
+++ salarios.js
@@ -0,0 +1,42 @@
+const colombia = [];
+
+colombia.push({
+    name: "Camila",
+    salary: 500,
+});
+colombia.push({
+    name: "Camila",
+    salary: 1500,
+});
+colombia.push({
+    name: "Nath",
+    salary: 1800,
+});
+colombia.push({
+    name: "Luisa",
+    salary: 1600,
+});
+colombia.push({
+    name: "Camilo",
+    salary: 1100,
+});
+colombia.push({
+    name: "Daniel",
+    salary: 2400,
+});
+colombia.push({
+    name: "Alejo",
+    salary: 3000,
+});
+colombia.push({
+    name: "Chris",
+    salary: 5000,
+});
+colombia.push({
+    name: "Juan",
+    salary: 800,
+});
+colombia.push({
+    name: "German",
+    salary: 1400,
+});
